21aa5f838bc7e3ad41f4d770b9461539f018928c
thejayadad <74875595+thejayadad@users.noreply.github.com>
2025-02-16 17:28:31 -0500

    update

--- lib/actions/post/delete-imageurl.ts
+++ lib/actions/post/delete-imageurl.ts
@@ -1,7 +1,7 @@
-'use server'
 // 'use server';
 
 import { prisma } from '@/lib/prisma';
+import { del } from '@vercel/blob';
 
 export const deleteImageUrl = async (postId: string) => {
   if (!postId) {
@@ -9,10 +9,27 @@ export const deleteImageUrl = async (postId: string) => {
   }
 
   try {
+    // Retrieve the current imageUrl from the database
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { imageUrl: true },
+    });
+
+    if (!post || !post.imageUrl) {
+      throw new Error('Post or image not found');
+    }
+
+    const imageUrl = post.imageUrl;
+
+    // Delete the image from Vercel Blob storage
+    await del(imageUrl);
+
+    // Update the post's imageUrl to null in the database
     const updatedPost = await prisma.post.update({
       where: { id: postId },
-      data: { imageUrl: null }, // Set imageUrl to null or '' to remove the image
+      data: { imageUrl: null },
     });
+
     return updatedPost;
   } catch (error) {
     console.error('Error deleting post image:', error);
